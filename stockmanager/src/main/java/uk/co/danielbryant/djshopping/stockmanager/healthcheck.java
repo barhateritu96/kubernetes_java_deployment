ab7a5160e476ebf5d5532e7a2893d4aff1caffa1
Ritu <103950525+barhateritu96@users.noreply.github.com>
2023-11-30 16:46:04 +0530

    Update healthcheck.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
@@ -20,13 +20,9 @@ public class SimpleRestController implements HealthIndicator {
     @Override
     public Health health() {
         try {
-            // Add your custom health check logic here
-            // If everything is healthy, return Health.up()
             return Health.up().build();
         } catch (Exception e) {
-            // Log the exception for debugging purposes
             e.printStackTrace();
-            // If there's an exception or a failure in your logic, return Health.down()
             return Health.down().withDetail("error", e.getMessage()).build();
         }
     }
@@ -34,13 +30,9 @@ public class SimpleRestController implements HealthIndicator {
     @GetMapping("/health")
     public ResponseEntity<String> healthEndpoint() {
         try {
-            // Add your custom health check logic here
-            // If everything is healthy, return ResponseEntity.ok()
             return ResponseEntity.ok().build();
         } catch (Exception e) {
-            // Log the exception for debugging purposes
             e.printStackTrace();
-            // If there's an exception or a failure in your logic, return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
             return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(e.getMessage());
         }
     }
