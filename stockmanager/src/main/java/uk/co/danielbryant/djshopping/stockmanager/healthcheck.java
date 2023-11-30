a46bb74fb42ea0c0090d8fe854e70eae5561a43a
Ritu <103950525+barhateritu96@users.noreply.github.com>
2023-11-30 14:53:09 +0530

    Update healthcheck.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
@@ -1,13 +1,47 @@
-// package com.example.springbootmanagementexample;
- 
-// import java.util.Date;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RestController;
- 
-// @RestController
-// public class SimpleRestController {
-//     @GetMapping("/example")
-//     public String example() {
-//         return "Hello User !! " + new Date();
-//     }
-// }
+package com.example.springbootmanagementexample;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class SimpleRestController implements HealthIndicator {
+
+    @GetMapping("/example")
+    public String example() {
+        return "Hello User !! " + new Date();
+    }
+
+    @Override
+    public Health health() {
+        try {
+            // Add your custom health check logic here
+            // If everything is healthy, return Health.up()
+            return Health.up().build();
+        } catch (Exception e) {
+            // Log the exception for debugging purposes
+            e.printStackTrace();
+            // If there's an exception or a failure in your logic, return Health.down()
+            return Health.down().withDetail("error", e.getMessage()).build();
+        }
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthEndpoint() {
+        try {
+            // Add your custom health check logic here
+            // If everything is healthy, return ResponseEntity.ok()
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            // Log the exception for debugging purposes
+            e.printStackTrace();
+            // If there's an exception or a failure in your logic, return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(e.getMessage());
+        }
+    }
+}
